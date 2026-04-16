915a43623b64652359ae8b9baacc135468826c84
Nungari Muchiru <nungari@Nungari.localdomain>
2026-04-16 08:14:56 +0300

    Completed lab and passed all tests

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-
-
+const combineUsers = (...allArrays) => {
+  return {
+    users: [].concat(...allArrays),
+    merge_date: new Date().toLocaleDateString()
+  };
+};
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
